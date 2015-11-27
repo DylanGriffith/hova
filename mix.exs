edb316b231eadca94455ac19a4c7ba66573a9e65
Dylan Griffith <dyl.griffith@gmail.com>
2015-11-27 21:17:29 +1100

    add eex to included_applications

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule Hova.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :cowboy, :plug],
+    included_applications: [:eex],
      mod: {Hova, []}]
   end
 
