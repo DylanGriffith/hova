4325a2037e530e4a596cbd72dde560052a40dfcd
Dylan Griffith <dyl.griffith@gmail.com>
2015-11-27 21:06:33 +1100

    use ex file for sentences

--- lib/hova/sentence.ex
+++ lib/hova/sentence.ex
@@ -2,13 +2,7 @@ defmodule Hova.Sentence do
   def start_link do
     {:ok, agent} = Agent.start_link(fn -> [] end, name: __MODULE__)
 
-    {:ok, all_text} = File.read("./lib/lyrics.txt")
-
-    sentences = String.split(all_text, "\n")
-      |> Enum.map(fn (s) -> String.strip(s) end)
-      |> Enum.filter(fn (s) -> String.length(s) > 5 end)
-
-    Agent.update(agent, fn _ -> sentences end)
+    Agent.update(agent, fn _ -> Hova.Data.sentences end)
 
     {:ok, agent}
   end
